afb0862c8c223200f4930ddd7d3191289926200c
rikerik <bako.erik.dominik@gmail.com>
2023-08-15 11:50:22 +0200

    Conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,23 @@
 function ListGroup() {
+    let items = [
+        'New York',
+        'Budapest',
+        'Tokyo',
+        'Paris',
+        'London'
+    ];
+    items = [];
+
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+        <>
+            <h1>List</h1>
+            {items.length === 0 && <p> No item found</p>}
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </>
     );
 }
 
